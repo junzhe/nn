01a8110dcf808b1e299621818898750c2e038659
junzhe <junzhe@umich.edu>
2014-04-23 23:47:36 -0400

    start nn

---
+++ nn_initialize.m
@@ -0,0 +1,9 @@
+function nn = nn_initialize(opts)
+	nn.layers = opts.layers;
+	nn.levels = numel(nn.layers);
+
+	for i = 2 : nn.levels
+		nn.W{i - 1} = (rand(nn.size(i), nn.size(i - 1)+1) - 0.5) * 2 * 4 * sqrt(6 / (nn.size(i) + nn.size(i - 1)));
+        	nn.vW{i - 1} = zeros(size(nn.W{i - 1}));
+	end
+end
